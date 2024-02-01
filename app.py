9d6f7771c5e258052839e6f23b19093b7cc54ca2
justdatascientist@gmail.com <justdatascientist@gmail.com>
2024-02-01 00:37:59 -0300

    infra banco de dados, classes modelos

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route("/teste")
-def teste():
-    return render_template('teste.html')
+
+
+
 
 if __name__ == '__main__':
     app.run()
-
-
--- models.py
+++ models.py
@@ -7,6 +7,24 @@ class User(db.Model):
     age = db.Column(db.Integer, nullable=False)
     full_name = db.Column(db.String(100), nullable=False)
 
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Date, nullable=False)
+    has_disease = db.Column(db.String(100), nullable=True)
+    smoke = db.Column(db.Boolean, nullable=False)
+    quit_smoking = db.Column(db.Boolean, nullable=False)
+    drink_rarely = db.Column(db.Boolean, nullable=False)
+    drink = db.Column(db.Boolean, nullable=False)
+    have_cancer = db.Column(db.Boolean, nullable=False)
+    history_of_cancer = db.Column(db.Boolean, nullable=False)
+    went_dentist = db.Column(db.Boolean, nullable=False)
+    consume_mate = db.Column(db.Boolean, nullable=False)
+    sunscreen = db.Column(db.Boolean, nullable=False)
+    sunstroke = db.Column(db.Boolean, nullable=False)
+    skin_lesion = db.Column(db.Boolean, nullable=False)
+
+    user_mail = db.Column(db.String(100), db.ForeignKey('user.email'), nullable=False)
+
 
 with app.app_context():
     db.create_all()
